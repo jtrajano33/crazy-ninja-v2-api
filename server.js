2fa0efb07b3098e3f45a8702c16dad2537fcb8dd
Justin Trajano <justintra@cloudstaff.com>
2019-06-19 07:06:23 +0800

    added start script for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors")
 
 const players = require("./routes/api/players");
 
